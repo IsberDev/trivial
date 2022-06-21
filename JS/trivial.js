6e0b55257dd078d1860fe387fe627e24380e8f42
Ismael Garcia Bermejo <IsmaelGB86@gmail.com>
2022-06-21 20:57:11 +0200

    Pirmera estrutura

--- JS/trivial.js
+++ JS/trivial.js
@@ -1,1 +1,17 @@
 "use strict";
+
+async function mainjson() {
+  const errorarea = document.querySelector("p.error");
+  try {
+    const resjson = await fetch("./quiz.json");
+    if (resjson.ok) {
+      const data = await resjson.json();
+      console.log(data[0]);
+    }
+  } catch (error) {
+    console.log(error.message);
+    errorarea.textContent = error.message;
+  }
+}
+
+mainjson();
