3fd0e4f55ca70fdac4cb40662e407d26d2ef140b
Ismael Garcia Bermejo <IsmaelGB86@gmail.com>
2022-06-22 21:51:51 +0200

    guardado de player en local storage

--- JS/trivial.js
+++ JS/trivial.js
@@ -1,20 +1,77 @@
 "use strict";
 
+// funcion para gestionar error el cual se gestina con un panel de errores de html y llamada del json
 async function mainjson() {
   const errorarea = document.querySelector("p.error");
   try {
     const resjson = await fetch("./quiz.json");
     if (resjson.ok) {
       const data = await resjson.json();
-      console.log(data[0]);
+      //console.log(data[0]);
     }
+    //throw new Error("Mi error");
   } catch (error) {
-    console.log(error.message);
+    //console.log(error.message);
     errorarea.textContent = error.message;
   }
 }
 
 mainjson();
+//------------------------------------------------------------------------
+
+const form = document.forms.User;
+
+form.addEventListener("submit", (e) => {
+  // Prevenimos que se envie el formulario al pulsar el botón.
+  e.preventDefault();
+  const player = form.elements.User;
+  // Creamos una nueva entrada para localstore con la variable player user con el texto del input.
+  addPlayer(player.value);
+  // Vaciamos el input.
+  player.value = "";
+});
+
+const saveuser = () => {
+  // Convertimos a una cadena de texto JSON el array de tareas.
+  const userjson = JSON.stringify(usuario.user);
+
+  // Guardamos las tareas en el local storage.
+  window.localStorage.setItem("Player", userjson);
+};
+
+const addPlayer = (text) => {
+  usuario.user.push({ text: text });
+  // Guardamos los cambios en el localStorage.
+  saveuser();
+};
+
+const usuario = {
+  user: [],
+  arraynunber: [],
+  acierto: [],
+  fallo: [],
+};
+
+const deleteAlluser = () => {
+  // Vaciamos el array de tareas.
+  usuario.user = [];
+
+  // Guardamos los cambios en el localStorage.
+  saveTasks();
+};
+
+//Obtenemos las tareas almacenadas en el Local Storage (si las hay).
+const localStorageuser = window.localStorage.getItem("Player");
+// * Si en la constante anterior recibimos un valor, JavaScript se tomará
+//  * eso como un true (la variable NO está vacía). En este caso nos quedamos
+//  * con lo que haya en el localStorage.
+//  *
+//  * De lo contrario, si localStorageTasks está vacío, "tasks" será igual
+//  * a un array vacío.
+const respartida = {
+  user: localStorageuser ? JSON.parse(localStorageuser) : [],
+};
+//-----------------------------------------------------------
 
 const start = document.querySelector(".start");
 const center = document.querySelector(".center");
@@ -63,3 +120,21 @@ function main() {
 }
 
 main();
+
+const localStoragewser = window.localStorage.getItem("Player");
+
+// const deletelocal = () => {
+//   // Vaciamos el array de tareas.
+//   Player = [];
+
+//   // Guardamos los cambios en el localStorage.
+//   saveTasks();
+// };
+
+// const saveTasks = () => {
+//   // Convertimos a una cadena de texto JSON el array de tareas.
+//   const tasksJSON = JSON.stringify(State.tasks);
+
+//   // Guardamos las tareas en el local storage.
+//   window.localStorage.setItem("tasks", tasksJSON);
+// };
